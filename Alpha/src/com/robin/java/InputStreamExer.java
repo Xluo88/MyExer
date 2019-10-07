c9e07c6582fb0766d441c81e2448b1895b61a76d
Robin <271558528@qq.com>
2019-10-07 17:58:25 +0800

    Update InputStreamExer.java

--- Alpha/src/com/robin/java/InputStreamExer.java
+++ Alpha/src/com/robin/java/InputStreamExer.java
@@ -3,7 +3,7 @@ package com.robin.java;
 public class InputStreamExer {
     public static void main(String[] args) {
 
-        System.out.println("Hello-world");
+        System.out.println("Hello-world-1000");
 
     }
 }
